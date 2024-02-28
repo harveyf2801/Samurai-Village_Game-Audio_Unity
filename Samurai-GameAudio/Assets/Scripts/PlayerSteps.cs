36ecc7bcc2dda9c00f2ce28a97e7312ad99a5b2c
harveyf2801 <47286380+harveyf2801@users.noreply.github.com>
2024-02-28 14:26:42 +0000

    Added footstep terrain params (ISSUES)
    Has some issues selecting the terrain correctly.

--- Samurai-GameAudio/Assets/Scripts/PlayerSteps.cs
+++ Samurai-GameAudio/Assets/Scripts/PlayerSteps.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerSteps : MonoBehaviour
 {
+    private enum CURRENT_TERRAIN:int { GRASS = 0, GRAVEL = 1, PATH = 2, WOOD = 3, SAND = 4 };
+
+    [SerializeField]
+    private CURRENT_TERRAIN currentTerrain;
+
     public FMOD.Studio.EventInstance footsteps;
     // Start is called before the first frame update
     void Start()
@@ -12,16 +17,59 @@ public class PlayerSteps : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        UpdateTerrain();
+    }
+
+    private void UpdateTerrain()
+    {
+        RaycastHit[] hit;
+
+        hit = Physics.RaycastAll(transform.position, Vector3.down, 10.0f);
+
+        foreach (RaycastHit rayhit in hit)
+        {
+            if (rayhit.transform.gameObject.layer == LayerMask.NameToLayer("Grass"))
+            {
+                currentTerrain = CURRENT_TERRAIN.GRASS;
+                break;
+            }
+            else if (rayhit.transform.gameObject.layer == LayerMask.NameToLayer("Gravel"))
+            {
+                currentTerrain = CURRENT_TERRAIN.GRAVEL;
+                break;
+            }
+            else if (rayhit.transform.gameObject.layer == LayerMask.NameToLayer("Path"))
+            {
+                currentTerrain = CURRENT_TERRAIN.PATH;
+                break;
+            }
+            else if (rayhit.transform.gameObject.layer == LayerMask.NameToLayer("Wood"))
+            {
+                currentTerrain = CURRENT_TERRAIN.WOOD;
+                break;
+            }
+            else if (rayhit.transform.gameObject.layer == LayerMask.NameToLayer("Sand"))
+            {
+                currentTerrain = CURRENT_TERRAIN.SAND;
+                break;
+            }
+            
+            // else if (rayhit.transform.gameObject.layer == LayerMask.NameToLayer("Water"))
+            // {
+            //     currentTerrain = CURRENT_TERRAIN.WATER;
+            // }
+        }
     }
 
     private void PlayFootstep()
     {
-        Debug.Log("step");
+        Debug.Log("Ran on ");
+        Debug.Log(currentTerrain);
+
         footsteps = FMODUnity.RuntimeManager.CreateInstance("event:/Character/Footsteps");
-        footsteps.setParameterByName("Terrain", 1);
+        footsteps.setParameterByName("Terrain", (float)currentTerrain);
         footsteps.setParameterByName("RunCrawl", 1);
         footsteps.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
         footsteps.start();
